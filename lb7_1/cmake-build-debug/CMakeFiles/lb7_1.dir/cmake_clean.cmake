d91cf1c9c8a7aaed6e65ae981dbf3dd887dd7a88
Kateika-M <muromskaya.katya@mail.ru>
2021-05-27 18:26:46 +0300

    tasks

---
+++ lb7_1/cmake-build-debug/CMakeFiles/lb7_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lb7_1.dir/main.cpp.o"
+  "lb7_1"
+  "lb7_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lb7_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lb7_2/cmake-build-debug/CMakeFiles/lb7_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lb7_2.dir/main.cpp.o"
+  "lb7_2"
+  "lb7_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lb7_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
